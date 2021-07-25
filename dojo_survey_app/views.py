3b8cb5e646f38c92de15313e8958c89f127bc43d
Joseph Giacobbe <josephgiacobbe@sudojoe.com>
2021-07-25 16:17:34 -0400

    revisited

--- dojo_survey_app/views.py
+++ dojo_survey_app/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "form.html")
 
+def formdata(request):
+        name = request.POST["name"]
+        location = request.POST["location"]
+        favlang = request.POST["favlang"]
+        comments = request.POST["comments"]
+        return redirect('/')
+
 def formsubmit(request):
-    if request.method == "POST":
-        things = {
-            "name": request.POST["name"],
-            "location": request.POST["location"],
-            "favlang": request.POST["favlang"],
-            "comments": request.POST["comments"],
-        }
-        return render(request, "results.html", things)
+        request.session['name'] = request.POST['name']
+        request.session['location'] = request.POST['location']
+        request.session['favlang'] = request.POST['favlang']
+        request.session['comments'] = request.POST['comments']
+        return render(request, "results.html")
 
 # Create your views here.
